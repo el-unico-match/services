a09af95cc02dba971c1c9fd5c0d6c9aa80753f0b
Martín Hernán <hgmart@users.noreply.github.com>
2024-05-15 01:41:48 -0300

    Coveral integration (#6)
    * Integrate coveral to repository
    
    * Agregar init py para que coveral pueda encontrar los tests
    
    * Removes incorrect import
    
    ---------
    
    Co-authored-by: Nicolas <nzanardo@fi.uba.ar>

--- tests/testServices.py
+++ tests/testServices.py
@@ -1,20 +1,8 @@
 import unittest
 # from mongomock import MongoClient as MongoClientMock
-from src.repository.database import repository
-
 
 class TestStringMethods(unittest.TestCase):
 
     def test_the_returned_is_the_expedted_one(self):
         return True
-
-
-        # p_send_message(123,321,11,"Hollo")
-        # from_to = p_chat_history(123,321)
-        # self.assertEqual(
-        #     { "chats": [
-        #         {"user_from": 123, "user_to":321, "timestamp":11, "message":"Hollo"}
-        #     ] },
-        #     from_to
-        # )
     
