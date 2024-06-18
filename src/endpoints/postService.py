fe36ef1421bf94b5afd8abd76b12e9f58415fad7
Martin Hernan Gomez <mhgomez@fi.uba.ar>
2024-06-17 23:51:47 -0300

    Adds enpoint to create an ApiKey

--- src/endpoints/postService.py
+++ src/endpoints/postService.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel, Field
 from repository.servicesMetadata import ApiService, ApiServicesStatus, ApiServicesTypes
 from repository.serviceRepository import ServiceRepository
 from dataclasses import dataclass
+from common.JwtHelper import createToken
 
 @dataclass
 class PostServiceRequest(BaseModel):
@@ -13,14 +14,42 @@ class PostServiceRequest(BaseModel):
    version: Optional[str] = Field()
    availability: ApiServicesStatus = Field()
 
-async def postService(request: PostServiceRequest) -> ApiService: 
+@dataclass
+class PostServiceResponse():
+   id: str
+   type: str
+   key: str
+   token: str
+   baseUrl: str
+   version: str
+   created: datetime
+   availability: str
+
+async def postService(request: PostServiceRequest) -> PostServiceResponse: 
+
+   token=createToken(request.baseUrl, request.type.value)
+
+   jwtParts=token.split('.')
+
    data=request.model_dump()   
    data["key"]="key"
    data["created"]=datetime.now()
    data["id"]=str(ObjectId())
+   data["key"]=jwtParts[1][:6]
 
    apiService=ApiService(**data)
    await ServiceRepository.persistItem(apiService)
    
-   return apiService
+   response=PostServiceResponse(
+      id=apiService.id,
+      type=apiService.type.value,
+      key=apiService.key,
+      token=token,
+      baseUrl=apiService.baseUrl,
+      version=apiService.version,
+      created=apiService.created,
+      availability=apiService.availability.value,
+   )
+   
+   return response
     
