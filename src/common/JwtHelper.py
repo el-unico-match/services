cf5eef1282fc575e4b497c93ca86dce3563b7776
Martín Hernán <hgmart@users.noreply.github.com>
2024-06-18 00:14:05 -0300

    7 crear apikey en api (#8)
    * Enables to connect to an external database
    
    * Changes all routes to services
    
    * Adds enpoint to create an ApiKey
    
    * Updates env variables

---
+++ src/common/JwtHelper.py
@@ -0,0 +1,17 @@
+import jwt
+from datetime import datetime, timedelta, timezone
+from configs.settings import settings
+
+def createToken(baseUrl: str, type: str):
+    payload = {
+        'baseUrl': baseUrl,
+        'type': type,
+        'exp': datetime.now(timezone.utc) + timedelta(hours=1)
+    }
+
+    secret_key = settings.jwt_private_key
+    algorithm = settings.jwt_algorithm
+
+    token = jwt.encode(payload, secret_key, algorithm=algorithm)
+
+    return token
